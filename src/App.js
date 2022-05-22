404f102e7eaea93b3289e02a6c26445ce950e7ed
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-22 11:05:18 +0600

    Initialized

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
